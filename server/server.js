ba9ee1b003105f0dedb44c73cf440d8f67a3f27f
Shravani <shravanispathak@gmail.com>
2025-10-18 15:30:16 +0530

    Implemented log in functionality on frontend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 const app=express()
+
+app.use(cors)
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({
